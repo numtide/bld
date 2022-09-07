ece52725521615d5b65c48fffe05903a1d4a9052
Jean-François Roche <jfroche@affinitic.be>
2022-09-07 12:41:08 +0200

    feat(cli): configure nix build options (#1)
    We want to be able to define some of the nix build options.

--- default.nix
+++ default.nix
@@ -1,7 +1,10 @@
-{ nixpkgs, ... }:
+{ nixpkgs, restrict_eval ? true, allow-import-from-derivation ? false, ... }:
 with nixpkgs;
+let
+  bldScript = builtins.replaceStrings [ "restrict-eval true" "restrict-eval ${builtins.toString restrict_eval}" ] [ "allow-import-from-derivation false" "allow-import-from-derivation ${builtins.toString allow-import-from-derivation}" ] (builtins.readFile ./bld.sh);
+in
 writeShellApplication {
   name = "bld";
   runtimeInputs = [ nix ];
-  text = builtins.readFile ./bld.sh;
+  text = bldScript;
 }
