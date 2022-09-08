c39ddd4fa7c95fc95bc8e3117c756130bddc9d5d
zimbatm <zimbatm@zimbatm.com>
2022-09-08 15:48:21 +0200

    add flake

--- default.nix
+++ default.nix
@@ -1,4 +1,7 @@
-{ nixpkgs, restrict_eval ? true, allow-import-from-derivation ? false, ... }:
+{ nixpkgs ? import <nixpkgs> { }
+, restrict_eval ? true
+, allow-import-from-derivation ? false
+}:
 with nixpkgs;
 let
   bldScript = builtins.replaceStrings [ "restrict-eval true" "restrict-eval ${builtins.toString restrict_eval}" ] [ "allow-import-from-derivation false" "allow-import-from-derivation ${builtins.toString allow-import-from-derivation}" ] (builtins.readFile ./bld.sh);
