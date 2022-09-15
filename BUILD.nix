ed6e79209896f50972c88f8e8bca8227f945c83e
Jean-François Roche <jfroche@affinitic.be>
2022-09-15 18:37:51 +0200

    fix: simplify build options
    We are going to use a config file to configure nix build

--- BUILD.nix
+++ BUILD.nix
@@ -1,13 +1,11 @@
 { self
 , nixpkgs
 , inputs
-, restrict_eval
-, allow-import-from-derivation
 , ...
 }:
 with nixpkgs;
 let
-  bldScript = builtins.replaceStrings [ "restrict-eval true" "restrict-eval ${toString restrict_eval}" ] [ "allow-import-from-derivation false" "allow-import-from-derivation ${toString allow-import-from-derivation}" ] (builtins.readFile ./bld.sh);
+  bldScript = builtins.readFile ./bld.sh;
   bin = writeShellApplication {
     name = "bld";
     runtimeInputs = [ nix fzf ];
