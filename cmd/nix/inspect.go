a7df7b6c41f7dcd2b8c955bd9c13e91cf7910e6f
Jean-François Roche <jfroche@affinitic.be>
2022-11-02 13:11:24 +0100

    feat: add inspect command
    Show package information: derivation details only for now

---
+++ cmd/nix/inspect.go
@@ -0,0 +1,64 @@
+package nix
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func Inspect(rootDirectory, target string) (err error) {
+	if err != nil {
+		errorMessage := fmt.Sprintf(
+			"Error looking for root directory %s", err.Error(),
+		)
+
+		return errors.New(errorMessage)
+	}
+
+	currentDirectory, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	path, attr, err := TargetToAttr(rootDirectory, currentDirectory, target)
+	if err != nil {
+		errorMessage := fmt.Sprintf(
+			"Error mapping target to directory %s", err.Error(),
+		)
+
+		return errors.New(errorMessage)
+	}
+
+	pathToBuild := strings.Trim(fmt.Sprintf("%s/%s", path, attr), "/")
+	pathToBuild = strings.Replace(pathToBuild, "/", ".", -1)
+
+	log.WithFields(log.Fields{"pathToBuild": pathToBuild}).Debug("Running nix show-derivation")
+
+	var command string
+
+	if pathToBuild != "" {
+		command = fmt.Sprintf("nix show-derivation $(nix-instantiate --no-gc-warning --json -A %s )", pathToBuild)
+	} else {
+		command = "nix show-derivation $(nix-instantiate --no-gc-warning --json )"
+	}
+
+	cmd := exec.Command("bash", "-c", command)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err = cmd.Run()
+	if err != nil {
+		errorMessage := fmt.Sprintf(
+			"Error while inspect target: %s", err.Error(),
+		)
+
+		return errors.New(errorMessage)
+	}
+
+	return nil
+}
